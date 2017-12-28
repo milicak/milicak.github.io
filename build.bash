112731321759d2688ed8949956f1705fbf5b5821
milicak <ilicakme@gmail.com>
2017-12-28 08:22:05 +0300

    blacksea update

--- build.bash
+++ build.bash
@@ -1,3 +1,7 @@
 # first create a jupyter file in _data
 # then convert that file to markdown without input cell
 # ipython nbconvert --to markdown --template nocell.tpl Bodrumtsunami_dippings.ipynb
+
+
+# jekyll build
+# jekyll serve
